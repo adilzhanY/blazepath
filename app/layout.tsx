c4dec402338773a2e7037ae6d1a0009a31129c14
adilzhanY <adilzhanit@gmail.com>
2025-03-24 11:12:05 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "BlazePath",
+  description:
+    "Ignite your interview skills and blaze a trail to your dream job with BlazePath.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
