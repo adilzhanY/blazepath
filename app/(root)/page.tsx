a8ac5dd141656e87c50b830e53e2e6dfac267d2f
adilzhanY <adilzhanit@gmail.com>
2025-03-31 18:18:53 +0200

    Added home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,67 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import { dummyInterviews } from "@/public/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 const page = () => {
   return (
-    <div>
-      <div>
-        <div>Home pAGE</div>
-      </div>
-    </div>
+    <>
+      {/* Welcome Card */}
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>
+            Get Interview-Ready with AI-Powered Practice &
+            Feedback
+          </h2>
+          <p className="text-lg">
+            Practice on real interview questions & get
+            instant Feedback
+          </p>
+          <Button
+            asChild
+            className="btn-primary max-sm:w-full">
+            <Link href="/interview">
+              Start an Interview
+            </Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      {/* User's Interviews */}
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {/* <p>You haven&apos;t taken any interviews yet</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              {...interview}
+              key={interview.id}
+            />
+          ))}
+        </div>
+      </section>
+      {/* Available Interviews */}
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {/* <p>There are no interviews available</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              {...interview}
+              key={interview.id}
+            />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
